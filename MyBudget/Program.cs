e16c6c230cf3e42287c31a16bf8a9e0e76ac14fe
Chad Geidel <lemadchef@gmail.com>
2020-11-22 18:02:06 -0700

    Changed to November

--- MyBudget/Program.cs
+++ MyBudget/Program.cs
@@ -8,7 +8,7 @@ namespace MyBudget
         {
             var bi = new BudgetImporter();
             var readDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\OneDrive\\Budget\\Bank\\2020 Statements";
-            bi.ImportFiles(Months.August, readDir);
+            bi.ImportFiles(Months.November, readDir);
         }
     }
 }
