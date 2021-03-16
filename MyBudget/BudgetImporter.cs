32bdef8dcde95f945ea9df2e2d6a3442ec1170a8
Chad Geidel <lemadchef@gmail.com>
2021-03-15 21:03:27 -0600

    Changed tagging and output
    Playing with tagging for easier spreadsheet manipulation.

--- MyBudget/BudgetImporter.cs
+++ MyBudget/BudgetImporter.cs
@@ -10,17 +10,103 @@ namespace MyBudget
 {
     class BudgetImporter
     {
+        static readonly Dictionary <string, string> SearchTermMapping = new Dictionary<string, string>
+        {
+            // deposit
+            {"colorado state", "Income"},
+            // housing
+            {"mortgage", "Mortgage"}, 
+            // utilities
+            {"xcel", "Utilities"},
+            {"denver water", "Utilities"},
+            {"comcast", "Utilities"},
+            {"xfinity", "Utilities"},
+            {"vzwrlss", "Utilities"},
+            // insurance
+            {"northwestern mu", "Insurance"},
+            {"geico", "Insurance"},
+            // storage
+            {"clearhome", "Storage"},
+            // shopping
+            {"target", "Target"},
+            {"costco whse", "Costco"},
+            {"amzn.com", "Amazon"},
+            {"sprouts", "Groceries"},
+            {"king", "Groceries"},
+            {"water - coffee", "Groceries"},
+            {"wholefds", "Groceries"},
+            {"safeway", "Groceries"},
+            // health
+            {"health", "Health"},
+            {"walgreens", "Health"},
+            {"salon velluto", "Health"},
+            {"deanna woodroff", "Health"},
+            {"ulta", "Health"},
+            // doggos
+            {"petsmart", "Doggos"},
+            {"krisers", "Doggos"},
+            {"petco", "Doggos"},
+            {"chewy", "Doggos"},
+            {"red rocks", "Doggos"},
+            {"animal care", "Doggos"},
+            // auto
+            {"gas", "Auto"},
+            {"ferney", "Auto"},
+            {"metro express", "Auto"},
+            {"co motor vehicle", "Auto"},
+            // dining out
+            {"tejado", "Restaurants"},
+            {"good times", "Restaurants"},
+            {"tasty house", "Restaurants"},
+            {"kaos", "Restaurants"},
+            {"illegal pete", "Restaurants"},
+            {"starbucks", "Restaurants"},
+            // booze
+            {"hb liquor", "Booze"},
+            {"total wine", "Booze"},
+            {"argonaut", "Booze"},
+            // apple
+            {"apple.com", "Apple"},
+            // streaming
+            {"netflix", "Streaming"},
+            {"youtube", "Streaming"},
+            // electronics
+            {"best buy", "Electronics"},
+            {"micro center", "Electronics"},
+            {"level 7", "Electronics"},
+            {"backblaze", "Electronics"},
+            {"iracing", "Electronics"},
+            // home improvement
+            {"ikea", "Home Improvement"},
+            {"cost plus wld", "Home Improvement"},
+            {"wm supercent", "Home Improvement"},
+            {"lowes", "Home Improvement"},
+            // clothes
+            {"everlane", "Clothes"},
+            {"mgemi", "Clothes"},
+            {"uniqlo", "Clothes"},
+            // atm
+            {"atm", "ATM"},
+            // shopping
+            {"ebay", "Shopping"},
+            {"fedex", "Shopping"}
+        };
+
+        private List<IBankExport> records;
+
+        public BudgetImporter()
+        {
+            records = new List<IBankExport>();
+        }
+
         public void ImportFiles(Months importMonth, string sourceDir)
         {
             var monthToImport = (int)importMonth;
             var di = new DirectoryInfo(sourceDir);
             var monthPattern = $"*{DateTime.Now.Year}-{monthToImport:D2}*";
-            var records = new List<IBankExport>();
-            
+
             foreach (var file in di.GetFiles(monthPattern))
             {
-                Console.WriteLine(file);
-
                 switch (file.Name)
                 {
                     case string bhfcuchk when bhfcuchk.Contains("bhfcu checking", StringComparison.OrdinalIgnoreCase):
@@ -40,22 +126,25 @@ namespace MyBudget
                         break;
                 }
             }
-
-            WriteToCsv(records, $"{DateTime.Now:yyyy}-{monthToImport:D2}-{DateTime.Now.ToFileTime()}.csv");
         }
 
-        private static void WriteToCsv(IEnumerable<IBankExport> records, string filename)
+        public void WriteToCsv(string filename)
         {
             using var writer = File.AppendText(filename);
-            writer.WriteLine("Date,Details,Debit,Credit,RecordType");
-            foreach (var record in records)
+            writer.WriteLine("Date,Details,Debit,Credit,RecordType,Bucket");
+            foreach (var record in records.OrderBy(r => r.Date))
             {
-                writer.WriteLine(record.ToCsvString());
+                writer.WriteLine(record.ToCsvString() + $",{GetBucket(record.Details)}");
             }
 
             // what am I doing wrong here?
             //var engine = new FileHelperEngine<IBankExport>();
             //engine.WriteFile($"CombinedRecords-{DateTime.Now.ToFileTime()}.csv", records);
+        }
+
+        private static string GetBucket(string description)
+        {
+            return SearchTermMapping.FirstOrDefault(kvp => description.ToLower().Contains(kvp.Key)).Value ?? string.Empty;
         }
 
         private T[] ReadBankFile<T>(string filename) where T : class
--- MyBudget/Program.cs
+++ MyBudget/Program.cs
@@ -8,7 +8,14 @@ namespace MyBudget
         {
             var bi = new BudgetImporter();
             var readDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\OneDrive\\Budget\\Bank\\2021 Statements";
+            Console.WriteLine("Importing January");
+            bi.ImportFiles(Months.January, readDir);
+            Console.WriteLine("Importing February");
             bi.ImportFiles(Months.February, readDir);
+            Console.WriteLine("Importing March");
+            bi.ImportFiles(Months.March, readDir);
+            Console.WriteLine("Finshed importing");
+            bi.WriteToCsv($"{DateTime.Now.Year}-cumulative-{DateTime.Now.ToFileTime()}.csv");
         }
     }
 }
