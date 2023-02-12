d35f8641c2ce53fb5777a92f010e307ddad45db7
Chad Geidel <chad_geidel@hotmail.com>
2023-02-12 15:07:13 -0700

    Added new terms to Term Mapping

--- MyBudget/TermMapping.cs
+++ MyBudget/TermMapping.cs
@@ -10,20 +10,47 @@ namespace MyBudget
             { "colorado state", "Income" },
             { "interest payment", "Income" },
             { "mobile deposit", "Income" }, // check deposit from iOS app
+            { "edeposit in branch", "Income" }, // check deposit from iOS app
+            { "geidel,barbara", "Income" }, // transfer from mom at BHFCU
+            { "deposit", "Income" }, // transfer from mom at BHFCU
+
             // tax
             { "irs treas", "Tax" }, // federal refund
             { "costtaxrfd", "Tax" }, // co state refund
-            { "manion", "Tax" },
+            { "manion", "Tax" }, // chad tax preparer
+            { "montue, woodard", "Tax" }, // dana tax preparer
+
             // cg consulting llc
             { "sos registration fee", "Biz" },
+
+            // bank transfer and payments
+            { "recurring transfer", "Bank" },
+            { "payment transfer", "Bank" },
+            { "online payment", "Bank" },
+            { "dda to dda", "Bank" },
+            { "online transfer", "Bank" },
+            { "from share 0001", "Bank" },
+            { "from share 0002", "Bank" },
+            { "to share 0001", "Bank" },
+            { "overdraft", "Bank" },
+            { "credit card auto pay", "Bank" },
+            { "acctverify", "Bank" },
+            { "harland clarke check", "Bank" },
+
             // cc payment
             { "to loan 0141", "CC Payment" }, // bhfcu cc
             { "to visa signature card xxxxxxxxxxxx5846", "CC Payment" }, // wf cc
             { "cash back redemption", "CC Payment" }, // wf cc cashback
+            { "payment thank you", "CC Payment" }, // wf cc
+            { "automatic payment", "CC Payment" }, // wf cc
+
             // cc interest
             { "interest charge", "CC Interest"}, // wf cc
-            // mortgage
-            { "mortgage", "Mortgage" },
+
+            // mortgage / rent
+            { "mortgage", "Mortgage Rent" },
+            { "turbotenan", "Mortgage Rent" },
+
             // utilities
             { "xcel", "Utilities" },
             { "denver water", "Utilities" },
@@ -31,15 +58,23 @@ namespace MyBudget
             { "xfinity", "Utilities" },
             { "vzwrlss", "Utilities" },
             { "cityofdenveronline", "Utilities" },
+            { "mobile 888-936-4968 pa", "Utilities" }, // xfinity mobile likely
+            { "denver 800-266-2278 co", "Utilities" }, // ??? $25 charge to BHFCU CC
+
             // insurance
             { "northwestern mu", "Insurance" },
             { "geico", "Insurance" },
+            { "agi*renters", "Insurance" },
+            { "transunion", "Insurance" },
+
             // storage
             { "clearhome", "Storage" },
+
             // individual stores,
             { "target", "Target" },
             { "costco whse", "Costco" },
             { "amzn.com", "Amazon" },
+
             // groceries
             { "sprouts", "Groceries" },
             { "king soop", "Groceries" },
@@ -47,11 +82,18 @@ namespace MyBudget
             { "wholefds", "Groceries" },
             { "safeway", "Groceries" },
             { "natural grocers", "Groceries" },
+            { "family fare", "Groceries" },
+            { "primo water", "Groceries" },
+
             // self-care
             { "salon velluto", "Self-care" },
+            { "perga", "Self-care" },
             { "ulta", "Self-care" },
             { "loreal", "Self-care" },
+            { "beautypie", "Self-care" },
             { "beauty pie", "Self-care" },
+            { "evolvetogether", "Self-care" },
+
             // medical
             { "health", "Health" },
             { "walgreens", "Health" },
@@ -67,6 +109,9 @@ namespace MyBudget
             { "uch ambulatory", "Health" },
             { "coast to coast comp", "Health" },
             { "coast to coast com", "Health" },
+            { "aesthetic nurse", "Health" },
+            { "pretavoir glasgow", "Health" },
+
             // doggos
             { "petsmart", "Doggos" },
             { "krisers", "Doggos" },
@@ -76,38 +121,53 @@ namespace MyBudget
             { "animal care", "Doggos" },
             { "quality paws", "Doggos" },
             { "fine pet", "Doggos" },
+            { "veterinary", "Doggos" },
+
+            // auto maintenance
+            { "ferney", "Auto Maint" },
+            { "co motor vehicle", "Auto Maint" },
+            { "air care colorado", "Auto Maint" },
+            { "black hills tire", "Auto Maint" },
+
             // auto
             { "gas", "Auto" },
             { "shell oil", "Auto" },
-            { "ferney", "Auto" },
             { "metro express", "Auto" },
-            { "co motor vehicle", "Auto" },
             { "prkg denver", "Auto" },
             { "prkg metr denver", "Auto" },
             { "7-eleven", "Auto" },
             { "circle k", "Auto" },
             { "conoco", "Auto" },
             { "parkwell", "Auto" },
-            { "air care colorado", "Auto" },
             { "car wash", "Auto" },
             { "lyft", "Auto" },
             { "uber", "Auto" },
             { "pilot", "Auto" },
-            { "black hills tire", "Auto" },
             { "lotus", "Auto" },
             { "frroogle", "Auto" }, // peak to peak miata club
             { "yesway", "Auto" },
             { "big d", "Auto" },
             { "johnson corner", "Auto" },
+            { "parking", "Auto" },
+            { "holiday stations", "Auto" },
+            { "phillips 66", "Auto" },
+            { "cenex", "Auto" },
+            { "loaf n jug", "Auto" },
+            { "kum&go", "Auto" },
+            { "arts complex garage", "Auto" },
+            { "autozone", "Auto" },
+            { "prairie auto", "Auto" },
+            { "oceans express", "Auto" },
+            { "s&s", "Auto" },
+
             // restaurants
             { "tejado", "Restaurants" },
             { "good times", "Restaurants" },
             { "tasty house", "Restaurants" },
             { "kaos", "Restaurants" },
             { "illegal pete", "Restaurants" },
-            { "starbucks", "Restaurants" },
-            { "corvus", "Restaurants" },
             { "snarfosaurus", "Restaurants" },
+            { "snarfs", "Restaurants" },
             { "five guys", "Restaurants" },
             { "rheinlander bakery", "Restaurants" },
             { "new image restaura", "Restaurants" },
@@ -118,9 +178,7 @@ namespace MyBudget
             { "tankfoods", "Restaurants" },
             { "santiagos", "Restaurants" },
             { "miyako", "Restaurants" },
-            { "dunkin", "Restaurants" },
             { "pizzeria", "Restaurants" },
-            { "coffee", "Restaurants" },
             { "carls jr", "Restaurants" },
             { "mcdonald's", "Restaurants" },
             { "2 penguins", "Restaurants" },
@@ -139,6 +197,46 @@ namespace MyBudget
             { "taqueria jalisco", "Restaurants" },
             { "trompeau", "Restaurants" },
             { "park & co", "Restaurants" },
+            { "qdoba", "Restaurants" },
+            { "la cocina", "Restaurants" },
+            { "subway", "Restaurants" },
+            { "cheese ranch", "Restaurants" },
+            { "gyro hub", "Restaurants" },
+            { "yum yum spice", "Restaurants" },
+            { "crave mediterranean", "Restaurants" },
+            { "pearl of siam", "Restaurants" },
+            { "gque", "Restaurants" },
+            { "noodles & co", "Restaurants" },
+            { "papa murphys", "Restaurants" },
+            { "papa murphy's", "Restaurants" },
+            { "chick-fil-a", "Restaurants" },
+            { "panera bread", "Restaurants" },
+            { "the spot cafe", "Restaurants" },
+            { "lucky bird", "Restaurants" },
+            { "little man ice", "Restaurants" },
+            { "in n out burger", "Restaurants" },
+            { "zoe ma ma", "Restaurants" },
+            { "asian bistro", "Restaurants" },
+            { "the bluegrass", "Restaurants" },
+            { "blue bonnet", "Restaurants" },
+            { "culvers", "Restaurants" },
+            { "clancy irish pub", "Restaurants" },
+            { "seamless.com", "Restaurants" },
+            { "the golden mill", "Restaurants" },
+            { "panaderia luna", "Restaurants" }, // bakery
+            { "walters 303", "Restaurants" },
+            { "two brothers idaho", "Restaurants" },
+            { "cilantroandperejil", "Restaurants" },
+            { "summer nights black hawk", "Restaurants" },
+
+            // coffee
+            { "coffee", "Coffee" },
+            { "dunkin", "Coffee" },
+            { "starbucks", "Coffee" },
+            { "corvus", "Coffee" },
+            { "bean fosters", "Coffee" },
+            { "tea street", "Coffee" },
+
             // booze
             { "liquor", "Booze" },
             { "total wine", "Booze" },
@@ -149,6 +247,19 @@ namespace MyBudget
             { "distillery", "Booze" },
             { "pint room", "Booze" },
             { "719-4345750", "Booze" },
+            { "lost cabin beer", "Booze" },
+            { "hay camp", "Booze" },
+            { "independent ale house", "Booze" },
+            { "tap house", "Booze" },
+            { "total beverage", "Booze" },
+            { "lowry beer garden", "Booze" },
+            { "bear valley wine", "Booze" },
+            { "barquentine", "Booze" },            
+            { "broncos pkwy wine", "Booze" },
+            { "edward's tobacco", "Booze" },
+            { "union lodge no. 1", "Booze" },
+            { "whispers bar", "Restaurants" },
+
             // entertainment
             { "netflix", "Entertainment" },
             { "youtube", "Entertainment" },
@@ -157,6 +268,15 @@ namespace MyBudget
             { "alamo", "Entertainment" },
             { "booksamillion", "Entertainment" },
             { "maximum fun", "Entertainment" },
+            { "bikram@maximu", "Entertainment" }, // also maximum fun podcast
+            { "hiveworks", "Entertainment" }, // zach weiner
+            { "tattered cover", "Entertainment" }, 
+            { "hulu", "Entertainment" },
+            { "nintendo", "Entertainment" },
+            { "amc", "Entertainment" },
+            { "deadly darling gifts", "Entertainment" }, // voicecoil music 
+            { "premium httpswww.news ma", "Entertainment" }, // something Chad bought from BHFCU CC
+
             // hobbies
             { "fancy tiger", "Hobbies" },
             { "iracing", "Hobbies" },
@@ -165,6 +285,10 @@ namespace MyBudget
             { "humblebundle", "Hobbies" },
             { "level 7", "Hobbies" },
             { "kickstarter.com", "Hobbies" },
+            { "etsy.com", "Hobbies" },
+            { "meininger art supp", "Hobbies" },
+            { "merchant and mills rye", "Hobbies" },
+            
             // electronics
             { "best buy", "Electronics" },
             { "micro center", "Electronics" },
@@ -174,12 +298,20 @@ namespace MyBudget
             { "apple store", "Electronics" },
             { "evga", "Electronics" },
             { "htc corp", "Entertainment" },
+            { "1password", "Entertainment" },
+            { "deciphertools.com", "Entertainment" },
+            { "microsoft*subscription", "Entertainment" },
+            
             // biking
             { "velosoul", "Biking" },
             { "roll massif", "Biking" },
             { "bikereg.com", "Biking" },
             { "outside events", "Biking" },
             { "allianzins.us", "Biking" }, // pretty sure this is insurance for bike ride
+            { "acme bicycles", "Biking" }, // rapid city
+            { "black hills bicycle", "Biking" }, // rapid city
+            { "scheels", "Biking" },
+
             // home improvement
             { "ikea", "Home Improvement" },
             { "cost plus wld", "Home Improvement" },
@@ -195,6 +327,8 @@ namespace MyBudget
             { "university hills a", "Home Improvement" }, // Ace Hardware in university Hills Plaza
             { "harbor freight", "Home Improvement" },
             { "ace hardware", "Home Improvement" }, 
+            { "best service company", "Home Improvement" }, 
+
             // clothes
             { "everlane", "Clothes" },
             { "mgemi", "Clothes" },
@@ -205,20 +339,35 @@ namespace MyBudget
             { "nordstrom", "Clothes" },
             { "baum-kuchen", "Clothes" },
             { "the orvis co", "Clothes" },
+            { "emmaonesock", "Clothes" },
+
             // shopping
             { "ebay", "Shopping" },
             { "fedex", "Shopping" },
             { "qvc", "Shopping" },
+            { "instacart", "Shopping" },
+            { "instacar", "Shopping" },
+            { "the ups store", "Shopping" },
+            { "usps", "Shopping" },
+
             // apple
             { "apple.com", "Apple" },
+
             // cash
             { "atm", "Cash" },
             { "venmo", "Cash" },
             { "apple cash", "Cash" },
+            { "withdrawal made", "Cash" },
+            { "etip", "Cash" },
+            { "international purchase transaction fee", "Cash" },
+
             // travel
             { "la quinta", "Travel" },
+            { "southwes", "Travel" },
+
             // legal
-            { "sh law", "Legal" }
+            { "sh law", "Legal" },
+            { "montgomery little soran", "Legal" }
         };
     }
 }
